490c8699f892e8dab33fea151af29b4da6ee7c1a
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-05 19:04:33 -0400

    Applab Compadibility?

--- src/scaler.js
+++ src/scaler.js
@@ -2,8 +2,17 @@
 const urlEmbed = `${location.search.match(/(?<=url=)[^&]*/g)}/embed`
 const embed = document.querySelector("#game");
 embed.src = urlEmbed;
-embed.style.height = urlEmbed.includes("gamelab") ? "400px": "450px";
+if(urlEmbed.includes("applab")) {
+  embed.style["position"] = "fixed";
+  embed.style["left"] = "50%"
+  embed.style["width"] = "356px"
+  embed.style["height"] = "515px"
+  embed.style["transform"] = "translate(-50%, -25%);"
+  embed.style["clip-path"] = "inset(68px, 0px, 0px, 36px)"
+else {
+  embed.style["height"] = "400px"
+}
 const aspect = Math.min(window.innerWidth, window.innerHeight);
 const scale = aspect / embed.style.height.match(/\d+/g)[0] - 0.05; // divide this by the height if you want the joypad
-embed.style["transform"] = `scale(${scale})`;
+embed.style["transform"] += `scale(${scale})`;
 embed.style["transform-origin"] = `top center`;
