2b4a2734be589149f8729bbfeadc01e9b63eaa77
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-05 19:20:22 -0400

    Applab Compadability #4

--- src/scaler.js
+++ src/scaler.js
@@ -7,12 +7,12 @@ if(urlEmbed.includes("applab")) {
   embed.style["left"] = "50%"
   embed.style["width"] = "356px"
   embed.style["height"] = "515px"
-  embed.style["transform"] += "translate(-50%, -25%);"
+  embed.style["transform"] = "translate(-50%, -25%);"
   embed.style["clip-path"] = "inset(68px 0px 0px 36px)"
 } else {
   embed.style["height"] = "400px"
 }
 const aspect = Math.min(window.innerWidth, window.innerHeight);
 const scale = aspect / embed.style.height.match(/\d+/g)[0] - 0.05; // divide this by the height if you want the joypad
-embed.style["transform"] += `scale(${scale})`;
+embed.style["transform"] = `${embed.style.transform}; scale(${scale})`;
 embed.style["transform-origin"] = `top center`;
