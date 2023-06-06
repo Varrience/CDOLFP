0fabcd8b23f4d7b2ed3a34234ca9d606a43d81df
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-05 20:50:44 -0400

    Update scaler.js

--- src/scaler.js
+++ src/scaler.js
@@ -1,6 +1,7 @@
 // Upscales CDO project, this will have to be dynamic depending on the projects aspect ratio
 const urlEmbed = `${location.search.match(/(?<=url=)[^&]*/g)}/embed`
 const embed = document.querySelector("#game");
+const aspect = Math.min(window.innerWidth, window.innerHeight);
 embed.src = urlEmbed;
 if(urlEmbed.includes("applab")) {
   embed.style["position"] = "fixed";
@@ -8,11 +9,11 @@ if(urlEmbed.includes("applab")) {
   embed.style["width"] = "356px"
   embed.style["height"] = "515px"
   embed.style["clip-path"] = "inset(68px 0px 0px 36px)"
-  embed.style["transform"] += "translate(-60%, -25%) "
+  embed.style["transform"] += "translate(-60%, -20%) "
+  const scale = aspect / (embed.style.height.match(/\d+/g)[0] - (embed.style.clipPath.match(/\d+/)[0] || 0)); // divide this by the height if you want the joypad
 } else {
   embed.style["height"] = "400px"
+  const scale = aspect / embed.style.height.match(/\d+/g)[0] - 0.05; // divide this by the height if you want the joypad
 }
-const aspect = Math.min(window.innerWidth, window.innerHeight);
-const scale = aspect / embed.style.height.match(/\d+/g)[0] - 0.05; // divide this by the height if you want the joypad
 embed.style["transform"] += `scale(${scale})`;
 embed.style["transform-origin"] = `top center`;
