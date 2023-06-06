9e0f1b9535664a096bb8de77273d3b9ff35afbd9
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-05 20:08:23 -0400

    Update scaler.js

--- src/scaler.js
+++ src/scaler.js
@@ -8,11 +8,11 @@ if(urlEmbed.includes("applab")) {
   embed.style["width"] = "356px"
   embed.style["height"] = "515px"
   embed.style["clip-path"] = "inset(68px 0px 0px 36px)"
-  //embed.style["transform"] += "translate(-50%, -10%);"
+  embed.style["transform"] += "translate(-50%, -10%) "
 } else {
   embed.style["height"] = "400px"
 }
 const aspect = Math.min(window.innerWidth, window.innerHeight);
 const scale = aspect / embed.style.height.match(/\d+/g)[0] - 0.05; // divide this by the height if you want the joypad
-embed.style["transform"] += `scale(${scale});`;
+embed.style["transform"] += `scale(${scale})`;
 embed.style["transform-origin"] = `top center`;
