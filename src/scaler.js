f91d9bfd1190364bdd16e844bc7ccc298016c560
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-26 22:54:19 -0400

    Update scaler.js

--- src/scaler.js
+++ src/scaler.js
@@ -3,6 +3,7 @@ const urlEmbed = `${location.search.match(/(?<=url=)[^&]*/g)}/embed`
 const embed = document.querySelector("#game");
 const aspect = Math.min(window.innerWidth, window.innerHeight);
 const id = urlEmbed.match(/.{43}(?=\/embed)/g)[0];
+let scale = 1;
 fetch(`https://fetch-proxy.jacobbutler6.repl.co/json?url=https://studio.code.org/v3/channels/${id}`)
 .then(response => {
   if (response.status < 206) {
@@ -23,11 +24,11 @@ if (urlEmbed.includes("applab")) {
   embed.style["width"] = "356px"
   embed.style["height"] = "515px"
   embed.style["clip-path"] = "inset(68px 0px 0px 36px)"
-  const scale = aspect / (embed.style.height.match(/\d+/g)[0] - (embed.style.clipPath.match(/\d+/)[0] || 0)) - 0.05; // divide this by the height if you want the joypad
+  scale = aspect / (embed.style.height.match(/\d+/g)[0] - (embed.style.clipPath.match(/\d+/)[0] || 0)) - 0.05; // divide this by the height if you want the joypad
   embed.style["transform"] = `translate(-60%, -21%) scale(${scale})`;
 } else {
   embed.style["height"] = "400px"
-  const scale = aspect / embed.style.height.match(/\d+/g)[0] - 0.05; // divide this by the height if you want the joypad
+  scale = aspect / embed.style.height.match(/\d+/g)[0] - 0.05; // divide this by the height if you want the joypad
   embed.style["transform"] = `scale(${scale})`;
 }
 embed.style["transform-origin"] = `top center`;
