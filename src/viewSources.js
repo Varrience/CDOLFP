839564eb89bb175d9e1d464aa8a49c5f3a14a467
Varrience <105261606+Varrience@users.noreply.github.com>
2023-06-27 09:10:34 -0400

    action button migration

---
+++ src/viewSources.js
@@ -0,0 +1,53 @@
+const sourceAPI = `https://fetch-proxy.jacobbutler6.repl.co/`;
+const hostAPI = `https://studio.code.org/v3/`;
+// Show any assets used outside of animations
+document.querySelector("#assets").onclick = function() {
+  let host = `${hostAPI}/assets/${id}`
+  fetch(`${sourceAPI}json?url=${host}`).then(response => {
+    if(response.status < 206) {
+      return response.json();
+    }
+  }).then(data => {
+    let content = "";
+    for(let media of data) {
+      content += `<p> ${media.filename} </p>`
+      if(media.category === "image") {
+        content += `<img src=${sourceAPI}media?url=${host}/${media.filename}></img>`
+      } else {
+        content += `
+        <audio controls>
+        <source src=${sourceAPI}media?url=${host}/${media.filename}></source>
+        </audio>`
+      }
+    }
+    document.querySelector("#content").innerHTML = content;
+  })
+}
+// Show all animations in a project
+document.querySelector("#animations").onclick = function() {
+  let host = `${hostAPI}animations/${id}`
+  fetch(`${sourceAPI}json?url=${host}`).then(response => {
+    if(response.status < 206) {
+      return response.json();
+    }
+  }).then(data => {
+    let content = "";
+    for(let image of data) {
+      if(image.filename !== undefined) {
+        content += `<img src=${sourceAPI}media?url=${host}/${image.filename}></image>`;
+      }
+    }
+    document.querySelector("#content").innerHTML = content;
+  })
+}
+// Show relevant info if it was published as a library
+document.querySelector("#libraries").onclick = function() {
+ let host = `${hostAPI}libraries/${id}`;
+ fetch(`${sourceAPI}json?url=${host}`).then(response => {
+    if(response.status < 206) {
+      return response.json();
+    }
+  }).then(data => {
+    document.querySelector("#content").innerHTML = `<code>${JSON.stringify(data)}</code>`
+  })
+}
