6a79d3eb7b3050a7f50ded250f3425b0fa0f3d65
Varrience <105261606+Varrience@users.noreply.github.com>
2026-01-19 14:32:10 -0500

    fix proxy link

--- src/projectFiller.js
+++ src/projectFiller.js
@@ -63,7 +63,7 @@ projectTable.innerHtml = "";
       <tr>
        <th>
         <a href=${location.pathname}project?url=https://studio.code.org${project.level}/${source}>
-         <img class=project-thumbnail src=https://proxy.corsfix.com/?https://corsproxy.io/?url=https://studio.code.org${project.thumbnailUrl} alt=${project.name}_Thumbnail loading=lazy></img>
+         <img class=project-thumbnail src=https://corsproxy.io?url=https://studio.code.org${project.thumbnailUrl} alt=${project.name}_Thumbnail loading=lazy></img>
         </a>
        </th>
        <th>
