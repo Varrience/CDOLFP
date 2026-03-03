ad686d266adf71c96376f3af3309cd4a791e2421
Varrience <105261606+Varrience@users.noreply.github.com>
2026-03-02 22:57:24 -0500

    try to fix more paths

--- src/viewSources.js
+++ src/viewSources.js
@@ -27,11 +27,11 @@ document.querySelector("#assets").onclick = function() {
     for(let media of data) {
       content += `<p> ${media.filename} </p>`
       if(media.category === "image") {
-        content += `<img src="${host}/${encodeURI(media.filename)}" style="max-width: 100%"></img>`
+        content += `<img src="${host}/${encodeURIComponent(media.filename)}" style="max-width: 100%"></img>`
       } else {
         content += `
         <audio controls>
-        <source src="${host}/${encodeURI(media.filename)}"></source>
+        <source src="${host}/${encodeURIComponent(media.filename)}"></source>
         </audio>`
       }
     }
@@ -49,7 +49,7 @@ document.querySelector("#animations").onclick = function() {
     let content = "";
     for(let image of data) {
       if(image.filename !== undefined) {
-        content += `<img src="${host}/${encodeURI(image.filename)}" style="max-width: 100%"></image>`;
+        content += `<img src="${host}/${encodeURIComponent(image.filename)}" style="max-width: 100%"></image>`;
       }
     }
     document.querySelector("#content").innerHTML = content || "<p> No Animations </p>";
